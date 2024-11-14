ce66accafec29656c76be610d19a1805e01bc9e8
MuneebAmdani <arhammuneeb239@gmail.com>
2024-11-14 12:44:42 +0500

    javascript second class

---
+++ app.js
@@ -0,0 +1,89 @@
+// // let name="muneeb";
+// // name="amdani";
+// // console.log(name)
+
+// // var name2="muneeb";
+// // name2 = "hanzala"
+// // console.log(name2)
+
+// // const name3="muneeb"
+// // name3="hanzala"
+// // console.log(name3)
+
+// //  let fullName='muneeb amdani' // data type string
+// //  let fullName2="muneeb amdani"
+
+// //  let age= 16; // data type number
+// //  let isMarried= true; // data type 
+
+// // let ispass=undefined;
+// // let isFeesPaid=null;
+
+// // let stringNumber="1234";
+// // console.log(typeof fullName)
+// // console.log(typeof age)
+// // console.log(typeof isMarried)
+// // console.log(typeof StringNumber)
+
+// // let num1=22;
+// // let num2=33;
+// // let num3="12"
+// // console.log(num1 + num2)
+// // console.log(num1 - num2)
+// // console.log(num2 - num3)
+// // console.log(num3 - num2)
+// // console.log(num3 + num2)
+// // console.log(num3 - num3)
+// // console.log(num3 * num3)
+
+// // let $name="ali"
+// // let _$name2="ali"
+// // console.log($name)
+
+// var popularNumber1 = 2 + 2;
+// var popularNumber2 = 12 + 24;
+// var popularNumber3 = 12 * 24;
+// var popularNumber4 = 12/24;
+// var popularNumber5 = 5 % 2;
+// console.log(popularNumber1)
+// console.log(popularNumber2)
+// console.log(popularNumber3)
+// console.log(popularNumber4)
+// console.log(popularNumber5)
+
+// var popularNumber1 = 2 +(3*4);
+// var popularNumber2 = 12 + 24;
+// var popularNumber3 = 12 * 24;
+// var popularNumber4 = 12/24;
+// var popularNumber5 = 5 % 2;
+// console.log(popularNumber1)
+// console.log(popularNumber2)
+// console.log(popularNumber3)
+// console.log(popularNumber4)
+// console.log(popularNumber5)
+
+//  let num=1;
+//   num++;
+//  console.log(num)
+
+//  let num=1;
+//  num=num+2;
+//   num++;
+//  console.log(num)
+
+//  let num=1;
+//  num=num+2;
+//   num++;
+//  console.log(num)
+
+ let num=1;
+ console.log(num++)
+ console.log(num)
+ 
+
+// let numFirst = prompt('Enter your first Name', 'Hanzala');
+// let numSecond = prompt("Enter your Second Name", ' Ahmed');
+
+// let add = numFirst + numSecond
+
+// document.getElementById("my-demo").innerHTML = 'Your Full Name: ' + add;
